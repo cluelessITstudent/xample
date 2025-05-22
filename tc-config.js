9e4d234ca8fdfe47c2608595065ae64c050f5386
cluelessITstudent <112802134+cluelessITstudent@users.noreply.github.com>
2025-05-21 22:13:31 -0700

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://5qK10-mbvCl10J4G2dU0y94Igv2b1MQI_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Instructional Designing 101 for Teachers"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
